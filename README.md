afee16b1bb5c5ff29ae5f084eea04dc050b2905f
Sarah S <stms15@mun.ca>
2023-06-07 13:55:11 -0230

    added a function to search spotify for query passed in

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PokePlaylist-app
+
+## Credits
+
+Because the fetch requests for Spotify were built inside functions, the browser wasn't waiting for a response from the API request before continuing to execute the code. To fix this, https://rapidapi.com/guides/fetch-api-async-await was referenced to turn these functions into "async" functions so that the browser would wait for a response before continuing.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // ------ Functions ------ //
 
-function getSpotifyAccessTokens() {
+async function getSpotifyAccessTokens() {
   var clientId = "4ebd5d061ce94134b7f2e6ee78d3fdbb";
   var clientSecret = "3bff6b09802b447d821ff9e4b28fb7dc";
   var reqURL = "https://accounts.spotify.com/api/token";
@@ -16,25 +16,74 @@ function getSpotifyAccessTokens() {
 
   var currentDateTime = dayjs();
 
-  fetch(reqURL, {
+  var response = await fetch(reqURL, {
     method: "POST",
     headers: reqHeaders,
     body: reqBody,
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var timeToExpire = data.expires_in;
-      data.expiryDate = currentDateTime
-        .add(timeToExpire, "second")
-        .format("YYYY-MM-DD HH:mm:ss");
-      localStorage.setItem("temporarySpotifyAccessToken", JSON.stringify(data));
-      console.log("Success: token received");
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  });
+
+  if (!response.ok) {
+    throw new Error("Error: " + response.status);
+  }
+  var data = await response.json();
+  var timeToExpire = data.expires_in;
+  data.expiryDate = currentDateTime
+    .add(timeToExpire, "second")
+    .format("YYYY-MM-DD HH:mm:ss");
+  localStorage.setItem("temporarySpotifyAccessToken", JSON.stringify(data));
+  console.log("Success: token received");
+}
+
+async function searchSpotify(query) {
+  var accessToken = JSON.parse(
+    localStorage.getItem("temporarySpotifyAccessToken")
+  );
+  var limit = "1";
+  var searchURL =
+    "https://api.spotify.com/v1/search?q=" +
+    query +
+    "&type=track&limit=" +
+    limit;
+  var searchHeaders = {
+    Authorization: accessToken.token_type + " " + accessToken.access_token,
+  };
+
+  var response = await fetch(searchURL, {
+    method: "GET",
+    headers: searchHeaders,
+  });
+
+  if (!response.ok) {
+    throw new Error("Error: " + response.status);
+  }
+  var data = await response.json();
+  var song = {
+    title: data.tracks.items[0].name,
+    album: {
+      title: data.tracks.items[0].album.name,
+      artwork: data.tracks.items[0].album.images[0],
+    },
+    artist: data.tracks.items[0].artists[0].name,
+    url: data.tracks.items[0].external_urls.spotify,
+  };
+
+  console.log(song);
+
+  var storedPlaylist = JSON.parse(localStorage.getItem("playlist"));
+  if (storedPlaylist == null || storedPlaylist.length >= 6) {
+    var playlist = [song];
+    localStorage.setItem("playlist", JSON.stringify(playlist));
+  } else {
+    storedPlaylist.push(song);
+    localStorage.setItem("playlist", JSON.stringify(storedPlaylist));
+  }
 }
 
 // ----------------------- //
+
+// Uncomment to test:
+
+// getSpotifyAccessTokens();
+// console.log(JSON.parse(localStorage.getItem("temporarySpotifyAccessToken")));
+// searchSpotify("water+mad");
+// console.log(JSON.parse(localStorage.getItem("playlist")));
