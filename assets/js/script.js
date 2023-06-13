8c0a858a447a9822d536760c4f3111b032b28ff9
Sarah S <stms15@mun.ca>
2023-06-13 19:30:11 -0230

    added some JS to display songs after search request

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,19 +67,67 @@ async function searchSpotify(query) {
     url: data.tracks.items[0].external_urls.spotify,
   };
 
-  console.log(song);
+  return song;
+}
 
-  var storedPlaylist = JSON.parse(localStorage.getItem("playlist"));
-  if (storedPlaylist == null || storedPlaylist.length >= 6) {
-    var playlist = [song];
-    localStorage.setItem("playlist", JSON.stringify(playlist));
-  } else {
-    storedPlaylist.push(song);
-    localStorage.setItem("playlist", JSON.stringify(storedPlaylist));
+function displayPlaylist(playlist) {
+  playlistDivEl = document.getElementById("playlist");
+  var count = 0;
+
+  console.log(playlist);
+  for (let song of playlist) {
+    console.log(song);
+    var songDivEl = document.createElement("div");
+    var songInfoDivEl = document.createElement("div");
+
+    var songTitleEl = document.createElement("h5");
+    var albumTitleEl = document.createElement("p");
+    var albumImgEl = document.createElement("img");
+    var artistEl = document.createElement("p");
+    var listenLinkEl = document.createElement("a");
+
+    songTitleEl.innerHTML = song.title;
+    albumTitleEl.innerHTML = song.album.title;
+    albumImgEl.src = song.album.artwork.url;
+    artistEl.innerHTML = song.artist;
+    listenLinkEl.innerHTML =
+      "Listen on Spotify <span class='fa'>&#xf08e;</span>";
+    listenLinkEl.href = song.url;
+    listenLinkEl.target = "_blank";
+
+    // add classes
+    songDivEl.setAttribute(
+      "class",
+      "d-flex flex-column flex-sm-row justify-content-start align-items-center mb-3"
+    );
+    songInfoDivEl.setAttribute("class", "d-flex flex-column ms-4");
+    albumImgEl.setAttribute("class", "album");
+    listenLinkEl.setAttribute("class", "spotify-link");
+
+    songInfoDivEl.appendChild(songTitleEl);
+    songInfoDivEl.appendChild(artistEl);
+    songInfoDivEl.appendChild(albumTitleEl);
+    songInfoDivEl.appendChild(listenLinkEl);
+
+    songDivEl.appendChild(albumImgEl);
+    songDivEl.appendChild(songInfoDivEl);
+    songDivEl.id = "song-" + (count + 1); // song element's id is "song-1", etc.
+    playlistDivEl.appendChild(songDivEl);
   }
 }
 
+async function generatePlaylistListener() {
+  songToAdd = await searchSpotify("fire");
+  songs.push(songToAdd);
+
+  displayPlaylist(songs);
+}
+
 // ----------------------- //
+
+var songs = [];
+
+generatePlaylistListener();
 
 // Notes about the the style of the Pokemon cards:
 //
